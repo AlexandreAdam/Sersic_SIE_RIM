42fbedf82aabba2c4a75e9b29e5f6ad4b590f3d1
Alexandre Adam <aadam@narval3.narval.calcul.quebec>
2022-04-27 13:59:57 -0400

    Deleted logs and fixed shell script to remove large batch sizes

--- cnn_gridsearch.sh
+++ cnn_gridsearch.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=3 # maximum cpu per task is 3.5 per gpus
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32G			     # memory per node
-#SBATCH --time=0-10:00		# time (DD-HH:MM)
+#SBATCH --time=1-00:00		# time (DD-HH:MM)
 #SBATCH --account=rrg-lplevass
 #SBATCH --job-name=Train_RIM_analytic_Gridsearch
 #SBATCH --output=%x-%j.out
@@ -18,14 +18,14 @@ python $ARIM/pretrain_cnn_gridsearch.py\
   --cnn_input_kernel_size 7 11\
   --cnn_filters 16 32\
   --cnn_activation relu tanh swish\
-  --batch_size 1 16 32 64 128\
-  --total_items 200000\
-  --epochs 100\
+  --batch_size 32\
+  --total_items 20000\
+  --epochs 1000\
   --optimizer adamax\
   --initial_learning_rate 1e-3 1e-4\
   --decay_rate 1 0.9 0.8\
   --decay_steps 10000\
-  --max_time 9.5\
+  --max_time 23.5\
   --checkpoints=10\
   --max_to_keep=1\
   --model_dir=$ARIM/models/\
